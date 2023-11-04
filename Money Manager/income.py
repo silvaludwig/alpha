fe8b05412cfe0c914b4a70825b34b4884ac685a2
silvaludwig <silvaludwig@outlook.com>
2023-11-03 21:20:01 -0300

    FINALLY!

--- Money Manager/income.py
+++ Money Manager/income.py
@@ -5,6 +5,7 @@ import tkinter
 
 
 class Income:
+    
     def __init__(self, source, value, recurrent, category, account):
         self.source = income_discrimination_entry.get()
         self.value = income_value_entry.get()
@@ -12,10 +13,20 @@ class Income:
         self.category = income_category_var.get()
         self.account = income_account_var.get()
 
+    def submit():
+            print('New income registered successfuly!')
+            print(f'Discrimination: {income_discrimination_entry.get()}')
+            print(f'Value: R${income_value_entry.get()}')
+            print(f'Date: {income_date_day_var.get()}/{income_date_month_var.get()}/{income_date_year_var.get()}')        
+            print(f'Recurrency: {income_recurrent_var.get()}')
+            print(f'Category: {income_category_var.get()}')
+            print(f'Account: {income_account_var.get()}')
+
+    # print(f'Source: {income_discrimination_entry}, Value: R${income_value_entry}, Recurrent: {income_recurrent_var}, \n Category: {income_category_var}, Account: {income_account_var} \n New income registered successfuly!')
+
+
 window = Tk()
 window.title('New Income')
-# frame = ttk.Frame(window, padding=10)
-# frame.grid()
 
 income_discrimination_label = ttk.Label(window, text='Discrimination: ')
 income_discrimination_label.grid(column=0, row=0)
@@ -50,10 +61,31 @@ month.grid(column=1, row=4)
 
 income_date_year_label = ttk.Label(window, text='Year')
 income_date_year_label.grid(column=0, row=5)
-income_date_year_entry = ttk.Entry(window)
-income_date_year_entry.grid(column=1, row=5)
+income_date_year_options = [2020, 2021, 2022, 2023, 2024]
+income_date_year_var = tkinter.StringVar()
+year = ttk.Combobox(window, values=income_date_year_options, textvariable=income_date_year_var)
+year.grid(column=1, row=5)
 
+income_category_var_label = ttk.Label(window, text='Category')
+income_category_var_label.grid(column=0, row=6)
+income_category_var_options = ['House', 'Car', 'Health', 'Pets', 'Taxes', 'Misc']
+income_category_var = tkinter.StringVar()
+category = ttk.Combobox(window, values=income_category_var_options, textvariable=income_category_var)
+category.grid(column=1, row=6)
 
+income_account_entry_label = ttk.Label(window, text='Account')
+income_account_entry_label.grid(column=0, row=7)
+income_account_var_options = ['Neon', 'Nubank', 'Nubank PJ', 'CEF', 'C6', 'C6 PJ', 'Wallet']
+income_account_var = tkinter.StringVar()
+account = ttk.Combobox(window, values=income_account_var_options, textvariable=income_account_var)
+account.grid(column=1, row=7)
+
+# submit button
+submit_button = ttk.Button(window, text='Submit', command= Income.submit)
+submit_button.grid(column=0, row=8, padx=10, pady=5)
+
+quit_button = ttk.Button(window, text='Quit', command=window.destroy)
+quit_button.grid(column=1, row=8)
 
 
 window.mainloop()
