1a66673184e54d97a9f8c8043ca9a21f52b48bb1
silvaludwig <silvaludwig@outlook.com>
2024-01-24 00:29:03 -0300

    multiple inheritance ex

--- Udemy_24/Python/classes/aula225/electronic.py
+++ Udemy_24/Python/classes/aula225/electronic.py
@@ -1,3 +1,6 @@
+from log import LogFileMixin, LogPrintMixin
+
+
 class Electronic:
     def __init__(self, name):
         self._name = name
@@ -7,12 +10,27 @@ class Electronic:
         if not self._turned_on:
             self._turned_on = True
 
-    def turn_of(self):
+    def turn_off(self):
         if not self._turned_on:
             self._turned_on = False
 
 
-class Smartphone(Electronic):
-    ...
+class Smartphone(Electronic, LogPrintMixin):
+    
+    def turn_on(self):
+        super().turn_on()
+    
+        if self._turned_on:
+            msg = f'{self._name} is turned on'
+            self.log_success(msg)
 
+
+    
+    def turn_off(self):
+        super().turn_off()
+        
+        if not self._turned_on:
+            msg = f'{self._name} is turned off'
+            self.log_success(msg)
+ 
 
--- Udemy_24/Python/classes/aula225/main.py
+++ Udemy_24/Python/classes/aula225/main.py
@@ -1,3 +1,11 @@
 from log import LogFileMixin, LogPrintMixin
+from electronic import Smartphone
+
+galaxy_note = Smartphone('Galaxy Note')
+iphone = Smartphone('iPhone 15')
+
+iphone.turn_off()
+galaxy_note.turn_on()
+
 
 
