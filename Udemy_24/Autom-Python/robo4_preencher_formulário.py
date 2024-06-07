66a7526e63cb0bc15fb3008f41d00d1e67a4527a
Ludwig S <silvaludwig@gmail.com>
2024-06-07 11:12:48 -0300

    finalização rotina semanal corrigindo bugs

--- Udemy_24/Autom-Python/robo4_preencher_formulário.py
+++ Udemy_24/Autom-Python/robo4_preencher_formulário.py
@@ -2,8 +2,6 @@ import pyautogui
 from time import sleep
 from selenium import webdriver
 from openpyxl import load_workbook
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
@@ -25,6 +23,7 @@ linha_pagina_amarela = len(pagina_formulario_amarela['B']) + 1
 print(pagina_login['A2'].value)
 print(pagina_login['B2'].value)
 
+
 navegador.get('https://institutoalexa.paineldoaluno.com.br/professor_login') # abrir o site
 sleep(2)
 navegador.find_element('xpath', '//*[@id="usuario"]').send_keys(pagina_login['A2'].value) # fazendo login pegando dados na planilha
@@ -35,56 +34,234 @@ sleep(1)
 navegador.find_element(By.XPATH, '//*[@id="esquerdo"]/ul/li[4]/a').click() # clicando em "aulas"
 sleep(5)
 
-# quarta feira
-for linha in range(2, len(pagina_formulario_amarela['B']) + 1): # passando por cada linha da coluna B e inserindo os dados nos campos correspondentes do formulario
-    navegador.find_element(By.XPATH, '//*[@id="dias_horarios_aulas"]/li/div/div[1]').click() # aula do topo, esquerda
-    sleep(2)
-    dados_a_preencher = pagina_formulario_amarela['B%s' % linha].value
-    navegador.find_element(By.XPATH, '//*[@id="conteudoMinistrado"]').send_keys(dados_a_preencher) # preenchendo conteudo ministrado
+
+def quarta_feira():    
+    navegador.find_element(By.XPATH, '//*[@id="dias_horarios_aulas"]/li/div/div[1]').click() # quarta feira, turma amarela manhã
+    sleep(5)
+    navegador.find_element(By.XPATH, '//*[@id="conteudoMinistrado"]').send_keys(pagina_formulario_amarela['B1'].value) # preenchendo conteudo ministrado
     sleep(1) 
-    navegador.find_element(By.XPATH, '//*[@id="objetivo_aprendizagem"]').send_keys(dados_a_preencher) # preenchendo objetivos de aprendizagem
+    navegador.find_element(By.XPATH, '//*[@id="objetivo_aprendizagem"]').send_keys(pagina_formulario_amarela['B2'].value) # preenchendo objetivos de aprendizagem
     sleep(1)
-    navegador.find_element(By.XPATH, '//*[@id="licao_de_casa"]').send_keys(dados_a_preencher) # preenchendo licao de casa
+    navegador.find_element(By.XPATH, '//*[@id="licao_de_casa"]').send_keys(pagina_formulario_amarela['B3'].value) # preenchendo licao de casa
     sleep(1)
-    navegador.find_element(By.XPATH, '//*[@id="metodologia"]').send_keys(dados_a_preencher) # preenchendo metodologia
+    navegador.find_element(By.XPATH, '//*[@id="metodologia"]').send_keys(pagina_formulario_amarela['B4'].value) # preenchendo metodologia
     sleep(1)
-    navegador.find_element(By.XPATH, '//*[@id="interacao"]').send_keys(dados_a_preencher) # preenchendo interação
+    navegador.find_element(By.XPATH, '//*[@id="interacao"]').send_keys(pagina_formulario_amarela['B5'].value) # preenchendo interação
     sleep(1)
-    navegador.find_element(By.XPATH, '//*[@id="participacao_estudantes"]').send_keys(dados_a_preencher) # preenchendo formas de registro
+    navegador.find_element(By.XPATH, '//*[@id="participacao_estudantes"]').send_keys(pagina_formulario_amarela['B6'].value) # preenchendo formas de registro
     sleep(1)
-    navegador.find_element(By.XPATH, '//*[@id="avaliacao_nao_presencial"]').send_keys(dados_a_preencher) # formas de avaliação
+    navegador.find_element(By.XPATH, '//*[@id="avaliacao_nao_presencial"]').send_keys(pagina_formulario_amarela['B7'].value) # formas de avaliação
     sleep(1)
     navegador.find_element(By.XPATH, '//*[@id="btnSalvar"]').click() # clicando em salvar
     sleep(2)
     navegador.find_element(By.XPATH, '/html/body/div[8]/div[3]/button').click() # confirmando salvar
-    break
-
-sleep(5)
-
-for linha_verde in range(2, len(pagina_formulario_verde['B']) + 1): 
-    navegador.find_element(By.XPATH, '//*[@id="dias_horarios_aulas"]/li/div/div[2]').click() # aula do topo, direita
     sleep(2)
-    dados_turma_verde = pagina_formulario_verde['B%s' % linha_verde].value
-    navegador.find_element(By.XPATH, '//*[@id="conteudoMinistrado"]').send_keys(dados_turma_verde) # preenchendo conteudo ministrado
+
+    navegador.find_element(By.XPATH, '//*[@id="dias_horarios_aulas"]/li/div/div[2]').click() # quarta feira, turma verde manhã
+    sleep(5)
+    navegador.find_element(By.XPATH, '//*[@id="conteudoMinistrado"]').send_keys(pagina_formulario_verde['B1'].value) # preenchendo conteudo ministrado
     sleep(1) 
-    navegador.find_element(By.XPATH, '//*[@id="objetivo_aprendizagem"]').send_keys(dados_turma_verde) # preenchendo objetivos de aprendizagem
+    navegador.find_element(By.XPATH, '//*[@id="objetivo_aprendizagem"]').send_keys(pagina_formulario_verde['B2'].value) # preenchendo objetivos de aprendizagem
     sleep(1)
-    navegador.find_element(By.XPATH, '//*[@id="licao_de_casa"]').send_keys(dados_turma_verde) # preenchendo licao de casa
+    navegador.find_element(By.XPATH, '//*[@id="licao_de_casa"]').send_keys(pagina_formulario_verde['B3'].value) # preenchendo licao de casa
     sleep(1)
-    navegador.find_element(By.XPATH, '//*[@id="metodologia"]').send_keys(dados_turma_verde) # preenchendo metodologia
+    navegador.find_element(By.XPATH, '//*[@id="metodologia"]').send_keys(pagina_formulario_verde['B4'].value) # preenchendo metodologia
     sleep(1)
-    navegador.find_element(By.XPATH, '//*[@id="interacao"]').send_keys(dados_turma_verde) # preenchendo interação
+    navegador.find_element(By.XPATH, '//*[@id="interacao"]').send_keys(pagina_formulario_verde['B5'].value) # preenchendo interação
     sleep(1)
-    navegador.find_element(By.XPATH, '//*[@id="participacao_estudantes"]').send_keys(dados_turma_verde) # preenchendo formas de registro
+    navegador.find_element(By.XPATH, '//*[@id="participacao_estudantes"]').send_keys(pagina_formulario_verde['B6'].value) # preenchendo formas de registro
     sleep(1)
-    navegador.find_element(By.XPATH, '//*[@id="avaliacao_nao_presencial"]').send_keys(dados_turma_verde) # formas de avaliação
-    break
+    navegador.find_element(By.XPATH, '//*[@id="avaliacao_nao_presencial"]').send_keys(pagina_formulario_verde['B7'].value) # formas de avaliação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="btnSalvar"]').click() # clicando em salvar
+    sleep(2)
+    navegador.find_element(By.XPATH, '/html/body/div[8]/div[3]/button').click() # confirmando salvar
+    sleep(2)
+    
+    navegador.find_element(By.XPATH, '//*[@id="dias_horarios_aulas"]/li/div/div[3]').click() # quarta feira, turma amarela tarde
+    sleep(5)
+    navegador.find_element(By.XPATH, '//*[@id="conteudoMinistrado"]').send_keys(pagina_formulario_amarela['B1'].value) # preenchendo conteudo ministrado
+    sleep(1) 
+    navegador.find_element(By.XPATH, '//*[@id="objetivo_aprendizagem"]').send_keys(pagina_formulario_amarela['B2'].value) # preenchendo objetivos de aprendizagem
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="licao_de_casa"]').send_keys(pagina_formulario_amarela['B3'].value) # preenchendo licao de casa
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="metodologia"]').send_keys(pagina_formulario_amarela['B4'].value) # preenchendo metodologia
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="interacao"]').send_keys(pagina_formulario_amarela['B5'].value) # preenchendo interação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="participacao_estudantes"]').send_keys(pagina_formulario_amarela['B6'].value) # preenchendo formas de registro
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="avaliacao_nao_presencial"]').send_keys(pagina_formulario_amarela['B7'].value) # formas de avaliação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="btnSalvar"]').click() # clicando em salvar
+    sleep(2)
+    navegador.find_element(By.XPATH, '/html/body/div[8]/div[3]/button').click() # confirmando salvar
+    sleep(2)
+    
+    navegador.find_element(By.XPATH, '//*[@id="dias_horarios_aulas"]/li/div/div[4]').click() # quarta feira, turma verde tarde
+    sleep(5)
+    navegador.find_element(By.XPATH, '//*[@id="conteudoMinistrado"]').send_keys(pagina_formulario_verde['B1'].value) # preenchendo conteudo ministrado
+    sleep(1) 
+    navegador.find_element(By.XPATH, '//*[@id="objetivo_aprendizagem"]').send_keys(pagina_formulario_verde['B2'].value) # preenchendo objetivos de aprendizagem
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="licao_de_casa"]').send_keys(pagina_formulario_verde['B3'].value) # preenchendo licao de casa
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="metodologia"]').send_keys(pagina_formulario_verde['B4'].value) # preenchendo metodologia
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="interacao"]').send_keys(pagina_formulario_verde['B5'].value) # preenchendo interação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="participacao_estudantes"]').send_keys(pagina_formulario_verde['B6'].value) # preenchendo formas de registro
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="avaliacao_nao_presencial"]').send_keys(pagina_formulario_verde['B7'].value) # formas de avaliação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="btnSalvar"]').click() # clicando em salvar
+    sleep(2)
+    navegador.find_element(By.XPATH, '/html/body/div[8]/div[3]/button').click() # confirmando salvar
+    sleep(5)
+    
+
+def terca_feira():    
+    navegador.find_element(By.XPATH, '//*[@id="dias_horarios_aulas"]/li/div/div[1]').click() # terça feira, turma verde manhã
+    sleep(5)
+    navegador.find_element(By.XPATH, '//*[@id="conteudoMinistrado"]').send_keys(pagina_formulario_verde['B1'].value) # preenchendo conteudo ministrado
+    sleep(1) 
+    navegador.find_element(By.XPATH, '//*[@id="objetivo_aprendizagem"]').send_keys(pagina_formulario_verde['B2'].value) # preenchendo objetivos de aprendizagem
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="licao_de_casa"]').send_keys(pagina_formulario_verde['B3'].value) # preenchendo licao de casa
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="metodologia"]').send_keys(pagina_formulario_verde['B4'].value) # preenchendo metodologia
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="interacao"]').send_keys(pagina_formulario_verde['B5'].value) # preenchendo interação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="participacao_estudantes"]').send_keys(pagina_formulario_verde['B6'].value) # preenchendo formas de registro
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="avaliacao_nao_presencial"]').send_keys(pagina_formulario_verde['B7'].value) # formas de avaliação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="btnSalvar"]').click() # clicando em salvar
+    sleep(2)
+    navegador.find_element(By.XPATH, '/html/body/div[8]/div[3]/button').click() # confirmando salvar
+    sleep(2)
+    
+    navegador.find_element(By.XPATH, '//*[@id="dias_horarios_aulas"]/li/div/div[2]').click() # terça feira, turma amarela manhã
+    sleep(5)
+    navegador.find_element(By.XPATH, '//*[@id="conteudoMinistrado"]').send_keys(pagina_formulario_amarela['B1'].value) # preenchendo conteudo ministrado
+    sleep(1) 
+    navegador.find_element(By.XPATH, '//*[@id="objetivo_aprendizagem"]').send_keys(pagina_formulario_amarela['B2'].value) # preenchendo objetivos de aprendizagem
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="licao_de_casa"]').send_keys(pagina_formulario_amarela['B3'].value) # preenchendo licao de casa
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="metodologia"]').send_keys(pagina_formulario_amarela['B4'].value) # preenchendo metodologia
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="interacao"]').send_keys(pagina_formulario_amarela['B5'].value) # preenchendo interação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="participacao_estudantes"]').send_keys(pagina_formulario_amarela['B6'].value) # preenchendo formas de registro
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="avaliacao_nao_presencial"]').send_keys(pagina_formulario_amarela['B7'].value) # formas de avaliação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="btnSalvar"]').click() # clicando em salvar
+    sleep(2)
+    navegador.find_element(By.XPATH, '/html/body/div[8]/div[3]/button').click() # confirmando salvar
+    sleep(2)
+
+    navegador.find_element(By.XPATH, '//*[@id="dias_horarios_aulas"]/li/div/div[3]').click() # quarta feira, turma amarela tarde
+    sleep(5)
+    navegador.find_element(By.XPATH, '//*[@id="conteudoMinistrado"]').send_keys(pagina_formulario_amarela['B1'].value) # preenchendo conteudo ministrado
+    sleep(1) 
+    navegador.find_element(By.XPATH, '//*[@id="objetivo_aprendizagem"]').send_keys(pagina_formulario_amarela['B2'].value) # preenchendo objetivos de aprendizagem
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="licao_de_casa"]').send_keys(pagina_formulario_amarela['B3'].value) # preenchendo licao de casa
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="metodologia"]').send_keys(pagina_formulario_amarela['B4'].value) # preenchendo metodologia
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="interacao"]').send_keys(pagina_formulario_amarela['B5'].value) # preenchendo interação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="participacao_estudantes"]').send_keys(pagina_formulario_amarela['B6'].value) # preenchendo formas de registro
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="avaliacao_nao_presencial"]').send_keys(pagina_formulario_amarela['B7'].value) # formas de avaliação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="btnSalvar"]').click() # clicando em salvar
+    sleep(2)
+    navegador.find_element(By.XPATH, '/html/body/div[8]/div[3]/button').click() # confirmando salvar
+    sleep(2)
+    
+    navegador.find_element(By.XPATH, '//*[@id="dias_horarios_aulas"]/li/div/div[4]').click() # terça feira, turma verde tarde
+    sleep(5)
+    navegador.find_element(By.XPATH, '//*[@id="conteudoMinistrado"]').send_keys(pagina_formulario_verde['B1'].value) # preenchendo conteudo ministrado
+    sleep(1) 
+    navegador.find_element(By.XPATH, '//*[@id="objetivo_aprendizagem"]').send_keys(pagina_formulario_verde['B2'].value) # preenchendo objetivos de aprendizagem
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="licao_de_casa"]').send_keys(pagina_formulario_verde['B3'].value) # preenchendo licao de casa
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="metodologia"]').send_keys(pagina_formulario_verde['B4'].value) # preenchendo metodologia
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="interacao"]').send_keys(pagina_formulario_verde['B5'].value) # preenchendo interação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="participacao_estudantes"]').send_keys(pagina_formulario_verde['B6'].value) # preenchendo formas de registro
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="avaliacao_nao_presencial"]').send_keys(pagina_formulario_verde['B7'].value) # formas de avaliação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="btnSalvar"]').click() # clicando em salvar
+    sleep(2)
+    navegador.find_element(By.XPATH, '/html/body/div[8]/div[3]/button').click() # confirmando salvar
+    sleep(5)
 
 
-# navegador.find_element(By.XPATH, '//*[@id="dias_horarios_aulas"]/li/div/div[3]').click() # baixo, esquerda
-# navegador.find_element(By.XPATH, '//*[@id="dias_horarios_aulas"]/li/div/div[4]').click() # baixo, direita
+def segunda_feira():
+    navegador.find_element(By.XPATH, '//*[@id="dias_horarios_aulas"]/li/div/div[1]').click() # segunda feira, repertório
+    sleep(5)
+    navegador.find_element(By.XPATH, '//*[@id="conteudoMinistrado"]').send_keys(pagina_formulario_repertorio['B1'].value) # preenchendo conteudo ministrado
+    sleep(1) 
+    navegador.find_element(By.XPATH, '//*[@id="objetivo_aprendizagem"]').send_keys(pagina_formulario_repertorio['B2'].value) # preenchendo objetivos de aprendizagem
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="licao_de_casa"]').send_keys(pagina_formulario_repertorio['B3'].value) # preenchendo licao de casa
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="metodologia"]').send_keys(pagina_formulario_repertorio['B4'].value) # preenchendo metodologia
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="interacao"]').send_keys(pagina_formulario_repertorio['B5'].value) # preenchendo interação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="participacao_estudantes"]').send_keys(pagina_formulario_repertorio['B6'].value) # preenchendo formas de registro
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="avaliacao_nao_presencial"]').send_keys(pagina_formulario_repertorio['B7'].value) # formas de avaliação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="btnSalvar"]').click() # clicando em salvar
+    sleep(2)
+    navegador.find_element(By.XPATH, '/html/body/div[8]/div[3]/button').click() # confirmando salvar
+    sleep(2)    
 
+    navegador.find_element(By.XPATH, '//*[@id="dias_horarios_aulas"]/li/div/div[2]').click() # segunda feira, turma azul noite
+    sleep(5)
+    navegador.find_element(By.XPATH, '//*[@id="conteudoMinistrado"]').send_keys(pagina_formulario_azul['B1'].value) # preenchendo conteudo ministrado
+    sleep(1) 
+    navegador.find_element(By.XPATH, '//*[@id="objetivo_aprendizagem"]').send_keys(pagina_formulario_azul['B2'].value) # preenchendo objetivos de aprendizagem
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="licao_de_casa"]').send_keys(pagina_formulario_azul['B3'].value) # preenchendo licao de casa
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="metodologia"]').send_keys(pagina_formulario_azul['B4'].value) # preenchendo metodologia
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="interacao"]').send_keys(pagina_formulario_azul['B5'].value) # preenchendo interação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="participacao_estudantes"]').send_keys(pagina_formulario_azul['B6'].value) # preenchendo formas de registro
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="avaliacao_nao_presencial"]').send_keys(pagina_formulario_azul['B7'].value) # formas de avaliação
+    sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="btnSalvar"]').click() # clicando em salvar
+    sleep(2)
+    navegador.find_element(By.XPATH, '/html/body/div[8]/div[3]/button').click() # confirmando salvar
+    sleep(5)
 
+def semana_completa():
+    quarta_feira() # chama a função e preenche os formulários de quarta feira
+    navegador.find_element(By.XPATH, '//*[@id="Capa_1"]').click() # clica em voltar pro dia de aula anterior, que no caso vai levar pra terça feira
+    sleep(5)
+    terca_feira() # chama a fução e preenche os formulários de terça feira
+    navegador.find_element(By.XPATH, '//*[@id="Capa_1"]').click() # clica em voltar pro dia de aula anterior, que no caso vai levar pra segunda feira
+    sleep(5)
+    segunda_feira() # a mesma coisa só que pra segunda 
+    navegador.find_element(By.XPATH, '//*[@id="Capa_1"]').click() # clica em voltar pro dia de aula anterior, que no caso vai levar pra quarta feira de novo
+    sleep(3)
+
+semana_completa()
 
 # fecha a janela e encerra o processo
 sleep(5)
