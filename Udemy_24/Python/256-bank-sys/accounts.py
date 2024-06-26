51857c85218e5ee18a499be1b948ef4a9b2a1789
Ludwig S <silvaludwig@outlook.com>
2024-06-26 17:47:32 -0300

    Savings account

--- Udemy_24/Python/256-bank-sys/accounts.py
+++ Udemy_24/Python/256-bank-sys/accounts.py
@@ -19,5 +19,23 @@ class Account(abc.ABC):
 
     def details(self, msg=''):
         print(f'Your balance: {self.balance:.2f} {msg}')
+        print('--')
 
 
+class SavingsAccount(Account):
+    def withdraw(self, amount):
+        amount_after_withdraw = self.balance - amount
+        if amount_after_withdraw >=0:
+            self.balance -= amount
+            self.details(f'(Withdraw {amount})')
+            return self.balance
+
+        print('Impossible to withdraw')
+        self.details(f'(Denied {amount})')
+
+
+if __name__ == '__main__':
+    savings_account_test_1 = SavingsAccount(111, 222, 0)
+    savings_account_test_1.withdraw(1)
+    savings_account_test_1.deposit(10)
+    savings_account_test_1.withdraw(5)
