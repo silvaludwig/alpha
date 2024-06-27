3d060169f7433821aea944d363ca7e7836ce54dc
Ludwig S <silvaludwig@outlook.com>
2024-06-27 10:58:00 -0300

    bank sys update

--- Udemy_24/Python/256-bank-sys/accounts.py
+++ Udemy_24/Python/256-bank-sys/accounts.py
@@ -33,9 +33,33 @@ class SavingsAccount(Account):
         print('Impossible to withdraw')
         self.details(f'(Denied {amount})')
 
+class CurrentAccount(Account):
+    def __init__(self, agency, account, balance=0, limit=0):
+        super().__init__(agency, account, balance)
+        self.limit = limit
+
+    def withdraw(self, amount):
+        amount_after_withdraw = self.balance - amount
+        max_limit = -self.limit
+
+        if amount_after_withdraw >= max_limit:
+            self.balance -= amount
+            self.details(f'(Withdraw {amount})')
+            return self.balance
+
+        print('Impossible to withdraw')
+        print(f'Your limit is {-self.limit:.2f}')
+        self.details(f'(Denied {amount})')
+
 
 if __name__ == '__main__':
     savings_account_test_1 = SavingsAccount(111, 222, 0)
     savings_account_test_1.withdraw(1)
     savings_account_test_1.deposit(10)
     savings_account_test_1.withdraw(5)
+    print('###')
+    current_account_test_1 = CurrentAccount(111, 222, 0, 100)
+    current_account_test_1.withdraw(1)
+    current_account_test_1.deposit(10)
+    current_account_test_1.withdraw(109)
+
